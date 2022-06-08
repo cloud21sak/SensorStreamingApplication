01585b5cc2b26a1bf3fc1e14b7eba3eac45d0103
norton123 <sakopov12@comcast.net>
2022-06-07 21:03:30 -0400

    Added test folders for remaining api lambdas

---
+++ 1-base-setup/2-sensorapp-api/functions/TestFolder/TestGetFacilitySensorConfig/testHarnessGetFacilitySensorConfig.js
@@ -0,0 +1,45 @@
+/*! Copyright Sergei Akopov (thecloud21.com). All Rights Reserved.
+ *  SPDX-License-Identifier: MIT-0
+ */
+
+const AWS = require("aws-sdk");
+
+// Mock event
+const event = require("./testEventGetCompletedProcesses.json");
+
+// Lambda handler
+const { handler } = require("../../getCompletedProcesses");
+const { deleteResourcesForTest } = require("./deleteTestResources.js");
+const {
+  initTestGetCompletedProcesses,
+} = require("./initTestGetFacilitySensorConfig.js");
+
+// Mock environment variables
+process.env.AWS_REGION = "us-east-1";
+AWS.config.region = process.env.AWS_REGION;
+process.env.DDB_TABLE = "sensordata-table";
+process.env.localTest = true;
+
+const main = async () => {
+  console.time("localTest");
+
+  // Initialize test resources:
+  console.log("Initializing test resources!");
+  await initTestGetCompletedProcesses();
+
+  // Testing lambda function:
+  console.log("Testing lambda!");
+  const readResult = await handler(event);
+  console.assert(
+    readResult.length === 2,
+    `Test failed! Expected # of items: 2, actual: ${readResult.length}`
+  );
+
+  // Delete the test resources:
+  console.log("Deleting test resources!");
+  await deleteResourcesForTest();
+
+  console.timeEnd("localTest");
+};
+
+main().catch((error) => console.error(error));
